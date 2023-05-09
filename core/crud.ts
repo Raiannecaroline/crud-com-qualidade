1123ae7b1adeb62de2e83d7af0748d6fe7ce13a6
Raiane Caroline S Teixeira <raianne.caroline@redeinova.net>
2023-05-09 18:15:46 -0300

    feat: Aula 2.1

--- core/crud.ts
+++ core/crud.ts
@@ -1,13 +1,15 @@
-const fs = require("fs");
+import fs from "fs"; //ES6
+
+// const fs = require("fs");
 const DB_FILE_PATH = "./core/db";
 
 console.log("[CRUD]");
 
-function create(content) {
+function create(content: string) {
   //Salvar no sistema
   fs.writeFileSync(DB_FILE_PATH, content);
   return content;
 }
 
 //Simulation
-console.log(create("Hoje eu preciso assistir as aulas!!"));
+console.log(create("Tenho que assistir as aulas e estudar!!"));
