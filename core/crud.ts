85994c41dd92ed0e4bee0bef9d39597ef70a80b1
Raiane Caroline S Teixeira <raianne.caroline@redeinova.net>
2023-05-16 16:39:54 -0300

    feat: Craiação de um Update

--- core/crud.ts
+++ core/crud.ts
@@ -1,15 +1,92 @@
 import fs from "fs"; //ES6
+import { v4 as uuidv4 } from 'uuid';
 
 // const fs = require("fs");
 const DB_FILE_PATH = "./core/db";
 
 console.log("[CRUD]");
 
-function create(content: string) {
+interface Todo {
+  id: string;
+  date: string;
+  content: string;
+  done: boolean;
+}
+
+function create(content: string): Todo {
+
+  const todo: Todo = {
+    id: uuidv4(),
+    date: new Date().toISOString(),
+    content: content,
+    done: false,
+  };
+
+  const todos: Array<Todo> = [
+    ...read(),
+    todo,
+  ];
+
   //Salvar no sistema
-  fs.writeFileSync(DB_FILE_PATH, content);
-  return content;
+  fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+    todos,
+    dogs: [],
+  }, null, 2));
+  return todo;
+}
+
+function read(): Array<Todo> {
+  const dbString = fs.readFileSync(DB_FILE_PATH, "utf-8");
+  const db = JSON.parse(dbString || "{}");
+  if (!db.todos) {
+    return [];
+  }
+
+  return db.todos;
+
+}
+
+
+//Update
+function update(id: string, partialTodo: Partial<Todo>): Todo {
+  let updatedTodo;
+  const todos = read();
+  todos.forEach((currentTodo => {
+    const isToUpdate = currentTodo.id === id;
+    if (isToUpdate) {
+      updatedTodo = Object.assign(currentTodo, partialTodo);
+    }
+  }));
+
+  fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+    todos,
+  }, null, 2));
+
+  if (!updatedTodo) {
+    throw new Error("Please, provider another ID")
+  }
+
+  return updatedTodo;
+}
+
+function updateContentById(id: string, content: string): Todo {
+  return update(id, {
+    content,
+  });
+}
+
+function clearDb() {
+  fs.writeFileSync(DB_FILE_PATH, "")
 }
 
 //Simulation
-console.log(create("Tenho que assistir as aulas e estudar!!"));
+clearDb();
+create("Primeira TODO");
+create("Primeira TODO");
+const terceiraTodo = create("Segunda TODO");
+// update(terceiraTodo.id, {
+//   content: "Atualizada",
+//   done: true,
+// });
+updateContentById(terceiraTodo.id, "Atualizada!!")
+console.log(read());
